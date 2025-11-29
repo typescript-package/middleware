9aaf2c4bb6bcc9ba9fc668370393b1622247fdb0
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-11-29 23:24:50 +0000

    test: update.

--- src/test/middleware.spec.ts
+++ src/test/middleware.spec.ts
@@ -3,28 +3,35 @@ import { Middleware } from "../lib";
 const middleware = new Middleware();
 
 middleware.use((args, next) => {
-  console.log('Middleware executed with args:', args);
+  console.log('Middleware 1 executed with args:', args);
+  args[0].newKey = 'newValue';
   next();
 });
 
 
 middleware.use((args, next) => {
-  console.log('Middleware executed with args:', args);
-  args[0].newKey = 'newValue';
+  console.log('Middleware 2 executed with args:', args);
   next();
 });
 
 middleware.execute({ key: 'value' });
 
+// Async
 middleware.use(async (args, next) => {
-  console.log('Async middleware start with args:', args);
-  // await new Promise(resolve => setTimeout(resolve, 2000));
+  console.log('Async middleware 1 start with args:', args);
+  args[0].newKey = 'newValue';
+  await new Promise(resolve => setTimeout(resolve, 2000));
   console.log('Async middleware end');
   next();
 });
 
+middleware.use(async (args, next) => {
+  console.log('Async middleware 2 start with args:', args);
+  next();
+});
+
 middleware.onComplete((args) => {
-  console.log('All middleware completed with args:', args);
+  console.log('All middlewares completed with args:', args);
 });
 
 middleware.executeAsync({ key: 'value' });
