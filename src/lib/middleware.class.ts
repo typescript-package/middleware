3cf501489a26ce0cf6348b97718a5b826e6b4265
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-11-30 19:00:25 +0000

    refactor(`Middleware`): use base abstraction.

--- src/lib/middleware.class.ts
+++ src/lib/middleware.class.ts
@@ -1,102 +1,19 @@
+// Abstract.
+import { MiddlewareBase } from './middleware-base.abstract';
 // Type.
 import { MiddlewareFunction } from '../type';
 /**
  * @description
  * @export
  * @class Middleware
+ * @typedef {Middleware}
  * @template [T=any] 
+ * @template {Function} [U=MiddlewareFunction<T>] 
+ * @extends {MiddlewareBase<T, T[], U>}
  */
-export class Middleware<T = any> {
-  /**
-   * @description
-   * @type {number}
-   */
-  #index = 0;
+export class Middleware<
+  T = any,
+  U extends Function = MiddlewareFunction<T>
+> extends MiddlewareBase<T, T[], U> {
 
-  /**
-   * @description
-   * @type {MiddlewareFunction[]}
-   */
-  #middleware: MiddlewareFunction[];
-
-  /**
-   * @description
-   * @type {() => void}
-   */
-  #onComplete: (args: T[]) => void = () => {};
-
-  /**
-   * Creates an instance of `ListenersMiddleware`.
-   * @constructor
-   * @param {...MiddlewareFunction[]} middleware 
-   */
-  constructor(...middleware: MiddlewareFunction[]) {
-    this.#middleware = middleware;
-  }
-
-  /**
-   * @description
-   * @public
-   * @param {...T[]} args 
-   */
-  public execute(...args: T[]) {
-    this.#index = 0;
-    this.#next(...args);
-  }
-
-  /**
-   * @description
-   * @public
-   * @async
-   * @param {...T[]} args 
-   * @returns {unknown} 
-   */
-  public async executeAsync(...args: T[]) {
-    this.#index = 0;
-    return new Promise<T[]>(async resolve => (
-      await this.#nextAsync(...args),
-      this.onComplete(() => resolve(args))
-    ));
-  }
-
-  /**
-   * @description
-   * @public
-   * @param {(args: T[]) => void} onComplete 
-   */
-  public onComplete(onComplete: (args: T[]) => void): void {
-    this.#onComplete = onComplete;
-  }
-
-  /**
-   * @description
-   * @public
-   * @param {MiddlewareFunction} middleware 
-   * @returns {this} 
-   */
-  public use(middleware: MiddlewareFunction): this {
-    return this.#middleware.push(middleware), this;
-  }
-
-  /**
-   * @description
-   * @param {...T[]} args 
-   */
-  #next(...args: T[]) {
-    this.#index < this.#middleware.length
-      ? this.#middleware[this.#index++](args, () => this.#next(...args))
-      : this.#onComplete(args);
-  }
-
-  /**
-   * @description
-   * @async
-   * @param {...T[]} args  
-   * @returns {*} 
-   */
-  async #nextAsync(...args: T[]) {
-    this.#index < this.#middleware.length
-      ? await this.#middleware[this.#index++](args, () => this.#nextAsync(...args))
-      : this.#onComplete(args);
-  }
 }
