cb8c68e94a26ede1f83aaef93bf2b4086957f5f2
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-12-01 15:35:34 +0000

    chore(api): update.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,7 @@
+// Class.
 export { Middleware } from './middleware.class';
+export { ContextMiddleware } from './context-middleware.class';
+// Abstract.
+export { ContextMiddlewareBase } from './context-middleware-base.abstract';
+export { MiddlewareBase } from './middleware-base.abstract';
+export { MiddlewareCore } from './middleware-core.abstract';
--- src/public-api.ts
+++ src/public-api.ts
@@ -1,7 +1,20 @@
 /*
  * Public API Surface of middleware
  */
-
 export {
-  Middleware
+  // Abstract.
+  MiddlewareCore,
+  // Base.
+  MiddlewareBase,
+  // Class.
+  Middleware,
 } from './lib';
+// Type.
+export type {
+  ContextMiddlewareFunction,
+  MiddlewareFunction,
+} from './type';
+// Interface.
+export type {
+  MiddlewareShape
+} from './interface'
