e7d818a9d2b2cc937d5acaf9af7bf3c39d4f9250
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-12-01 15:08:14 +0000

    feat: add context (object) based middleware for req, res.

---
+++ src/lib/context-middleware.class.ts
@@ -0,0 +1,16 @@
+// Abstract.
+import { ContextMiddlewareBase } from './context-middleware-base.abstract';
+// Type.
+import { ContextMiddlewareFunction } from '../type';
+/**
+ * @description
+ * @export
+ * @class ContextMiddleware
+ * @template [T=any] 
+ * @template {Function} [U=MiddlewareFunction<T>] 
+ * @extends {ContextMiddlewareBase<T, U>}
+ */
+export class ContextMiddleware<
+  T = any,
+  U extends Function = ContextMiddlewareFunction<T>
+> extends ContextMiddlewareBase<T, U> {}
